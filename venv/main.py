a0183ffbc91e1aa4b79e7e60e47ca684601ef30e
Itamar Hassin <ihassin@mac.com>
2019-03-11 13:05:29 -0400

    ignore

--- venv/main.py
+++ venv/main.py
@@ -1,6 +1,6 @@
 from markov import Markov, get_table
 
-m = Markov('a lot of added additions text in this sentence')
-b = m.predict('a')
+m = Markov('i will try to use this buggy program to predict what the next letter is')
+b = m.predict('b')
 
 print('<' + b + '>')
